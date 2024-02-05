adb5c2b423734c156a78bd944a2bef0379133bb1
tracysgit <tracy@wildiris.net>
2024-02-05 12:14:43 -0600

    Tweaked the header.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Metadata } from 'next';
 import { getCategories, getRecipes } from '@/app/lib/localdata';
 import { sortArrayOfObjAsc } from "@/app/utils/utils";
-import HeaderPage from '@/app/components/header-page/header-page';
+// import HeaderPage from '@/app/components/header-page/header-page';
+import Search from '@/app/components/search/search';
 import RecipesByCategory from '@/app/components/recipes/recipes-by-category.tsx';
 import { TCategories, TRecipes } from '@/app/lib/definitions';
+import H1Headline from './components/headlines/h1Headline';
 
 export const metadata: Metadata = {
   title: 'Home | Recipes',
@@ -16,7 +18,11 @@ export default async function HomePage() {
   
   return (
     <>
-      <HeaderPage h1Id="header-page__headline" h1Text="All Recipes" hasBreadcrumbs={false} hasSearch={false} />
+      <header>
+        {/* <Search recipes={recipesAscending} /> */}
+        <H1Headline id="header-page__headline">All Recipes</H1Headline>
+      </header>
+      {/* <HeaderPage h1Id="header-page__headline" h1Text="All Recipes" hasBreadcrumbs={false} hasSearch={false} /> */}
 
       <RecipesByCategory categories={categories} recipes={recipesAscending} deckClasses="mb-4 md:mb-8 lg:mb-10 mt-[5px]" showH2Headline={true} showFormatToggle={true} formatToggleClasses="mt-2 mb-6 md:mb-8 lg:mb-10" />
     </>
