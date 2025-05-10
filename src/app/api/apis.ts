786df91d6b707679dc99690ad9851b22b00bc973
tracysgit <tracy@wildiris.net>
2025-05-10 16:43:55 -0500

    Updated the API path.

--- src/app/api/apis.ts
+++ src/app/api/apis.ts
@@ -1,6 +1,8 @@
 import { IRecipe } from "../utils/types/types";
 
-const API_URL = "http://localhost:3000/api";
+//TODO This must be altered for Dev and Published
+// const API_URL = "http://localhost:3000/api";
+const API_URL = "https://recipes-react-next-tsx.vercel.app/api";
 
 /**
  * GetCategories
