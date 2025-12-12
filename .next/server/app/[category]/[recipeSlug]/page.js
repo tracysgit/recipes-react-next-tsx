69460b3e5be9a55472c15ab8c108aff9e6703c47
tracysgit <tracy@wildiris.net>
2025-12-12 13:09:48 -0600

    Security Update

--- .next/server/app/[category]/[recipeSlug]/page.js
+++ .next/server/app/[category]/[recipeSlug]/page.js
@@ -1,6 +1,6 @@
 const CHUNK_PUBLIC_PATH = "server/app/[category]/[recipeSlug]/page.js";
 const runtime = require("../../../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_c336f25b._.js");
+runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_0962b827._.js");
 runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__4c58c055._.js");
 runtime.loadChunk("server/chunks/ssr/src_app_a4430781._.js");
 runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__2f29c6f6._.js");
